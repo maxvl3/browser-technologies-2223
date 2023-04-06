f89ee2a70fec9560e572a945e03f868625493326
Max <94384526+maxvl3@users.noreply.github.com>
2023-04-06 11:02:14 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
-// Selecteer alle inputvelden en scorebars in het formulier
-const formElements = document.querySelectorAll(
-  'form input[type="text"], form input[type="email"], input[type="radio"]'
-);
+// Controleer of local storage beschikbaar is
+let storageAvailable = typeof Storage !== "undefined";
 
-// Loop door de inputvelden en scorebars heen en sla de ingevulde waarde op in local storage
-formElements.forEach((element) => {
-  // Voeg een event listener toe om de waarde op te slaan wanneer deze verandert
-  element.addEventListener("change", () => {
-    localStorage.setItem(element.name, element.value);
-  });
+if (storageAvailable) {
+  // Selecteer alle inputvelden en scorebars in het formulier
+  const formElements = document.querySelectorAll(
+    'form input[type="text"], form input[type="email"], input[type="radio"]'
+  );
 
-  // Haal de opgeslagen waarde op uit local storage en vul het element in met deze waarde
-  const savedValue = localStorage.getItem(element.name);
-  if (savedValue) {
-    if (element.type === "radio") {
-      if (element.value === savedValue) {
-        element.checked = true;
+  // Loop door de inputvelden en scorebars heen en sla de ingevulde waarde op in local storage
+  formElements.forEach((element) => {
+    // Voeg een event listener toe om de waarde op te slaan wanneer deze verandert
+    element.addEventListener("change", () => {
+      localStorage.setItem(element.name, element.value);
+    });
+
+    // Haal de opgeslagen waarde op uit local storage en vul het element in met deze waarde
+    const savedValue = localStorage.getItem(element.name);
+    if (savedValue) {
+      if (element.type === "radio") {
+        if (element.value === savedValue) {
+          element.checked = true;
+        }
+      } else {
+        element.value = savedValue;
       }
-    } else {
-      element.value = savedValue;
     }
-  }
-});
+  });
+} else {
+  // Als local storage niet beschikbaar is, voer hier passende acties uit, bijvoorbeeld een fallback-optie bieden
+  console.log("Local storage is niet beschikbaar.");
+}
